d31708ecd4d48f8a40cf5cba2040faf7888c6753
Eric_Hu <66540507+3b1b-sh@users.noreply.github.com>
2024-09-13 14:46:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -14,6 +14,60 @@ function generateProgressBar() {
 }
 
 const readme = `\
+<p align="center">
+<img src="https://capsule-render.vercel.app/api?type=waving&color=timeGradient&height=300&&section=header&text=Hi%20There&fontSize=90&fontAlign=50&fontAlignY=30&desc=I%20am%20Eric%20Hu&descAlign=50&descSize=30&descAlignY=60&animation=twinkling" />
+</p>
+
+[![Typing SVG](https://readme-typing-svg.demolab.com?font=Fira+Code&size=30&pause=1000&center=true&vCenter=true&multiline=true&width=1000&height=100&lines=Welcome+to+my+Github+profile+page!;I+hope+you+would+like+my+projects!)](https://git.io/typing-svg)
+
+<div style="display: flex; justify-content: center;">  
+    <img width="400" src="https://github-readme-stats.vercel.app/api?username=3b1b-sh&theme=buefy&include_all_commits=true&show_icons=true&hide_border=true" />  
+    <img width="400" src="https://streak-stats.demolab.com?user=3b1b-sh&hide_border=true" />  
+</div>
+
+<img width="800" src="https://github-readme-activity-graph.vercel.app/graph?username=3b1b-sh&theme=github-compact&hide_border=true&area=true" />
+
+<div style="text-align: center;">  
+    <img style="display: inline-block; margin: 0 10px;" src="https://github-readme-stats.vercel.app/api/wakatime?username=Epiphany_Resolution&theme=transparent&hide_border=true&layout=compact&langs_count=22" />  
+    <img style="display: inline-block; margin: 0 10px;" src="https://github-readme-stats.vercel.app/api/top-langs/?username=3b1b-sh&theme=transparent&hide_border=true&layout=donut-vertical&langs_count=7" />  
+</div>
+
+<div style="display: flex; justify-content: space-between;">  
+    <a href="https://github.com/3b1b-sh/Project-Performance-Evaluation-of-Bandit-Algorithms">  
+        <img src="https://github-readme-stats.vercel.app/api/pin/?username=3b1b-sh&repo=Project-Performance-Evaluation-of-Bandit-Algorithms&show_owner=true" alt="Readme Card">  
+    </a>  
+    <a href="https://github.com/3b1b-sh/Deep_learning_Dynamic_MRI_Reconstruction">  
+        <img src="https://github-readme-stats.vercel.app/api/pin/?username=3b1b-sh&repo=Deep_learning_Dynamic_MRI_Reconstruction&show_owner=true" alt="Readme Card">  
+    </a>  
+</div>
+
+<div style="display: flex; justify-content: space-between;">  
+    <a href="https://github.com/3b1b-sh/Deep_learning_Cardiac_Cine_MRI">  
+        <img src="https://github-readme-stats.vercel.app/api/pin/?username=3b1b-sh&repo=Deep_learning_Cardiac_Cine_MRI&show_owner=true" alt="Readme Card">  
+    </a>  
+    <a href="https://github.com/3b1b-sh/Building-a-toy-RVC-CPU">  
+        <img src="https://github-readme-stats.vercel.app/api/pin/?username=3b1b-sh&repo=Building-a-toy-RVC-CPU&show_owner=true" alt="Readme Card">  
+    </a>  
+</div>
+
+<div style="display: flex; justify-content: space-between;">  
+    <a href="https://github.com/3b1b-sh/Flappy-bird-game-on-Longan-Nano">  
+        <img src="https://github-readme-stats.vercel.app/api/pin/?username=3b1b-sh&repo=Flappy-bird-game-on-Longan-Nano&show_owner=true" alt="Readme Card">  
+    </a>  
+    <a href="https://github.com/3b1b-sh/2D_Self_Driving_Simulator">  
+        <img src="https://github-readme-stats.vercel.app/api/pin/?username=3b1b-sh&repo=2D_Self_Driving_Simulator&show_owner=true" alt="Readme Card">  
+    </a>  
+</div>
+
+<div align="center">  
+    <img src="https://skillicons.dev/icons?i=github,py,c,cpp,md,vscode,matlab,bash,git,gitlab,docker,linux,powershell&theme=light" />  
+</div>
+
+<p align="center">
+    <a href="https://github.com/3b1b-sh"><img src="https://img.shields.io/badge/GitHub-3b1b-sh?logo=github" /></a>
+    <img src="https://komarev.com/ghpvc/?username=3b1b-sh&abbreviated=true&color=yellow" />
+</p>
+
 ### Hi there 👋
 
 ⏳ Year progress ${progressBarOfThisYear} ${(progressOfThisYear * 100).toFixed(2)} %
@@ -22,6 +76,11 @@ const readme = `\
 
 ⏰ Updated on ${new Date().toUTCString()}
 
-![Progress Bar CI](https://github.com/liununu/liununu/workflows/Progress%20Bar%20CI/badge.svg)\
+![Progress Bar CI](https://github.com/liununu/liununu/workflows/Progress%20Bar%20CI/badge.svg)
+
+<p align="center">
+    <img src="https://capsule-render.vercel.app/api?type=waving&color=timeGradient&height=300&&section=footer&text=The%20End&fontSize=90&fontAlign=50&fontAlignY=70&desc=Hope%20%you%20have%20a%20nice%20day%20!&descAlign=50&descSize=30&descAlignY=40&animation=twinkling" />
+</p>
 `;
+
 console.log(readme);
